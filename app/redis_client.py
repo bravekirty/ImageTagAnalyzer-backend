4c5f2c136f76ed929557529d0b430c45cae2e843
Kir Tarasov <kir_tarasov@mail.ru>
2025-10-30 18:27:34 +0300

    🐛 Redis config

--- app/redis_client.py
+++ app/redis_client.py
@@ -5,7 +5,7 @@ from app.config import settings
 redis_client = redis.Redis(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
-    # password=settings.REDIS_PASSWORD,
+    password=settings.REDIS_PASSWORD,
     decode_responses=True,
 )
 
